4e590fb0d9715046f965d314ca7a0ebb668e5377
ronsinl <ronsinl@K129-19.dep-info.iutmontp.univ-montp2.fr>
2016-01-20 13:27:51 +0100

    Ajout d'une entrée fonctionnel

--- annuaire/src/annuaire/AddEntryListener.java
+++ annuaire/src/annuaire/AddEntryListener.java
@@ -2,7 +2,12 @@ package annuaire;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashSet;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.swing.JList;
 import javax.swing.JOptionPane;
@@ -12,10 +17,12 @@ public class AddEntryListener implements ActionListener {
 	
 	private boolean cleAjoutee = false;
 	private String s,s2;
+	private Properties contacts;
+	private String propertiesFielLocation = "myProperties.properties";
 	
 	public AddEntryListener()
 	{
-		
+		this.contacts = Model.getContacts();
 	}
 
 	@Override
@@ -32,6 +39,19 @@ public class AddEntryListener implements ActionListener {
 		{
 			s2 = (String)JOptionPane.showInputDialog(AddressBook.getFrame(), "Entrez les infos à ajouter", "Ajout d'une personne", JOptionPane.YES_NO_CANCEL_OPTION, null, null, null);
 		}
-		
+		Set<String> setString = new HashSet<String>();
+		setString.add(s);
+		setString.add(s2);
+		for(String a : setString)
+		{
+			contacts.setProperty(s, s2);
+		}
+		try (OutputStream out = new FileOutputStream(propertiesFielLocation))
+		{
+			contacts.store(out, "Fichier de propriétés");
+		}
+		catch(IOException e1) {
+			e1.printStackTrace();
+		}
 	}
 }
